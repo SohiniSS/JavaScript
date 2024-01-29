4a7ef573a0421862b5cb97d693194c51bd7de75e
SohiniSS <sohini.tonai@gmail.com>
2024-01-29 20:44:47 +0530

    html added

--- index.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello Debojit Sadhukhan")
-console.log("Hello world")
---
+++ main.js
@@ -0,0 +1,3 @@
+console.log("Hello world")
+document.write("This is from JS file.\n")
+document.write("\n Hello world from JS file")
