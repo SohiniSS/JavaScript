c13d3866ebf65db7dbc18e0a6f064c394b5ea614
SohiniSS <sohini.tonai@gmail.com>
2024-01-30 14:22:33 +0530

    StringNumberBoolean_Conversion

---
+++ 01_basic/dataTypeConversion.js
@@ -0,0 +1,20 @@
+let score = "33";
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+
+let point = "678"
+let conversionPoint = Number(point)
+console.log(conversionPoint)
+
+let isLoggedIn = ""
+let boolconversion = Boolean(isLoggedIn)
+console.log(boolconversion)
+// 1 => true    0 => false
+// "hello" => true
+// "" => false
+
+let anynumber = 23
+let convertToString = String(anynumber);
+console.log(typeof(convertToString))
---
+++ 01_basic/variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345678
+let accounEmail = "test@gmail.com"
+var accountPassword = 876
+accountCity = "Jaipur"
+
+// accountId = 2
+console.log(accountCity)
